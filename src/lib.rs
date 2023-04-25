3c8a1ad40306e4c40672115b970efb1cab9d16cf
Multirious <Multirious@outlook.com>
2023-04-25 19:28:34 +0700

    init

---
+++ src/lib.rs
@@ -0,0 +1,48 @@
+use std::collections::BTreeSet;
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize, Serialize)]
+pub struct Blocks {
+    pub blocks: Vec<Block>,
+}
+
+#[derive(Deserialize, Serialize)]
+pub struct Block {
+    pub name: String,
+    pub parameters: Vec<ParameterOrString>,
+    pub block_parameters: usize,
+}
+
+#[derive(Deserialize, Serialize)]
+pub enum ParameterOrString {
+    Parameter(Parameter),
+    String(String),
+}
+
+#[derive(Deserialize, Serialize)]
+pub enum Parameter {
+    Any,
+    Bool,
+    EnumOrAny {
+        possible_values: BTreeSet<String>,
+        restricted_values: BTreeSet<String>,
+    },
+    Enum {
+        possible_values: BTreeSet<String>,
+    },
+}
+
+#[derive(Deserialize, Serialize)]
+pub enum BlockType {
+    Hat,
+    Stack,
+    Cap,
+    Reporter,
+    Boolean,
+    C,
+}
+
+fn main() {
+    println!("Hello, world!");
+}
